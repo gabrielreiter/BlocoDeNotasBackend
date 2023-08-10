6c86138208172422fc383a07e7d4aa8dceffeb9a
gabrielreiter <132106468+gabrielreiter@users.noreply.github.com>
2023-08-09 22:13:28 -0300

    adding CorsMapping

--- Bloco-de-Notas/src/main/java/com/bloconotasgabreiter/BlocoDeNotas/BlocoDeNotasApplication.java
+++ Bloco-de-Notas/src/main/java/com/bloconotasgabreiter/BlocoDeNotas/BlocoDeNotasApplication.java
@@ -2,10 +2,18 @@ package com.bloconotasgabreiter.BlocoDeNotas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BlocoDeNotasApplication {
+public class BlocoDeNotasApplication implements WebMvcConfigurer{
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
+		
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BlocoDeNotasApplication.class, args);
 	}
