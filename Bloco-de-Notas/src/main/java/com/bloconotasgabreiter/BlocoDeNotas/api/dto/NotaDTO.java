2e9b9b32655d00f85ae537ee68256d67805f2c2d
gabrielreiter <132106468+gabrielreiter@users.noreply.github.com>
2023-08-23 01:21:52 -0300

    adding id to DTO

--- Bloco-de-Notas/src/main/java/com/bloconotasgabreiter/BlocoDeNotas/api/dto/NotaDTO.java
+++ Bloco-de-Notas/src/main/java/com/bloconotasgabreiter/BlocoDeNotas/api/dto/NotaDTO.java
@@ -4,16 +4,27 @@ import java.time.LocalDateTime;
 
 public class NotaDTO {
 
+	private Long id;
+	
 	private String titulo;
 
 	private String texto;
 
 	private LocalDateTime dataAtualizacao;
 
-	public NotaDTO(String titulo, String texto, LocalDateTime dataAtualizacao) {
+	public NotaDTO(Long id, String titulo, String texto, LocalDateTime dataAtualizacao) {
+		this.id = id;
 		this.titulo = titulo;
 		this.texto = texto;
 		this.dataAtualizacao = dataAtualizacao;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public LocalDateTime getDataAtualizacao() {
